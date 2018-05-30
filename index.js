768b53a01afc78a7cfe3c6a121a53c02a601a7c6
Dmitry Ivanov <dmitry.ivanov.od@gmail.com>
2018-05-31 00:37:40 +0300

    add post request

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var jsonServer = require('json-server');
+var bodyParser = require('body-parser')
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
+server.use(bodyParser.json())
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    next()
+})
+
 server.use(router);
 server.listen(port, function () {
     console.log('JSON Server is running')
